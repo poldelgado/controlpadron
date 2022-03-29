1e13545d0b814993aaa168cb40a14cb7daf09317
Pablo Delgado <poldelgado@gmail.com>
2022-03-29 19:58:17 -0300

    agrego componente card para resultados

---
+++ resources/js/components/CardResultado.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card text-center shadow-sm my-1 mx-1 ">
+        <div class="p-3">
+            <h3 class="fw-bold">Total</h3>
+            <h3 class="text-info fw-bold">{{tipo}}}</h3>
+            <h3 class="card-text display-3 cardData">{{total}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['tipo','total','color-class']
+}
+</script>
